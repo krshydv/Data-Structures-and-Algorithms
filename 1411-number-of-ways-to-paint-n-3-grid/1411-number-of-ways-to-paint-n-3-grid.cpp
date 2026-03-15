0ebeacd284ce7be471016a4c54049c86a3dc58e3
Krish Yadav <krishyadav8545@gmail.com>
2026-03-15 18:48:45 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.38%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numOfWays(int n) {
+        const long long MOD = 1e9 + 7;
+        
+        long long aba = 6;
+        long long abc = 6;
+        
+        for(int i = 2; i <= n; i++) {
+            long long new_aba = (aba * 3 + abc * 2) % MOD;
+            long long new_abc = (aba * 2 + abc * 2) % MOD;
+            
+            aba = new_aba;
+            abc = new_abc;
+        }
+        
+        return (aba + abc) % MOD;
+    }
+};
