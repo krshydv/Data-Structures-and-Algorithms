0bb8b54e6f922f76945a65cadaae72585be62f0a
Krish Yadav <krishyadav8545@gmail.com>
2026-03-19 21:16:03 +0530

    Time: 49 ms (70.73%), Space: 130.7 MB (22.76%) - LeetHub

---
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numberOfSubmatrices(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        
+        // Prefix counts of X and Y
+        vector<vector<int>> px(n + 1, vector<int>(m + 1, 0));
+        vector<vector<int>> py(n + 1, vector<int>(m + 1, 0));
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                px[i][j] = px[i - 1][j] + px[i][j - 1] - px[i - 1][j - 1] + (grid[i - 1][j - 1] == 'X');
+                py[i][j] = py[i - 1][j] + py[i][j - 1] - py[i - 1][j - 1] + (grid[i - 1][j - 1] == 'Y');
+            }
+        }
+        
+        int ans = 0;
+        
+        // Submatrices that contain grid[0][0] are exactly prefixes (0,0) -> (i,j)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int cx = px[i][j];
+                int cy = py[i][j];
+                
+                if (cx == cy && cx > 0) ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
