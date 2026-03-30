1bf5559147c1b914a48d8c15b6b7b3a7f71a9348
Krish Yadav <krishyadav8545@gmail.com>
2026-03-30 22:42:34 +0530

    Time: 6 ms (71.26%), Space: 19.9 MB (62.64%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        vector<int> e1(26), o1(26), e2(26), o2(26);
+        for (int i = 0; i < s1.size(); i++) {
+            if (i % 2 == 0) {
+                e1[s1[i] - 'a']++;
+                e2[s2[i] - 'a']++;
+            } else {
+                o1[s1[i] - 'a']++;
+                o2[s2[i] - 'a']++;
+            }
+        }
+        return e1 == e2 && o1 == o2;
+    }
+};
